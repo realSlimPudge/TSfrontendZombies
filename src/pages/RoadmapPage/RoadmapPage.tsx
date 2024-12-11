59a0e2da18cb41fdd4f0b99e28ce4c16d32264dc
realSlimPudge <komissarov010806@gmail.com>
2024-12-11 12:59:13 +0300

    roadmap v2

--- src/pages/RoadmapPage/RoadmapPage.tsx
+++ src/pages/RoadmapPage/RoadmapPage.tsx
@@ -1,37 +1,37 @@
-import React, { useEffect } from "react";
-import styles from "./RoadmapPage.module.scss";
-import { useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { getRoadmap } from "../../shared/slices/roadmapSlice";
-import { RootState } from "../../shared/store/store";
-import RoadMap from "../../processes/RoadMap/RoadMap";
+import React, { useEffect } from 'react'
+import styles from './RoadmapPage.module.scss'
+import { useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { getRoadmap } from '../../shared/slices/roadmapSlice'
+import { RootState } from '../../shared/store/store'
+import RoadMap from '../../processes/RoadMap/RoadMap'
 
 const Roadmap: React.FC = () => {
-    const { discipline } = useParams<{ discipline: string }>();
-    const { loading, error } = useSelector((state: RootState) => state.roadmap);
-    const dispatch = useDispatch();
-    useEffect(() => {
-        if (discipline) {
-            dispatch(getRoadmap(discipline));
-        }
-    }, [discipline, dispatch]);
+	const { discipline } = useParams<{ discipline: string }>()
+	const { loading, error } = useSelector((state: RootState) => state.roadmap)
+	const dispatch = useDispatch()
+	useEffect(() => {
+		if (discipline) {
+			dispatch(getRoadmap(discipline))
+		}
+	}, [discipline, dispatch])
 
-    if (loading) {
-        return <div>loading...</div>;
-    }
+	if (loading) {
+		return <div>loading...</div>
+	}
 
-    if (error) {
-        return <div>error:{error}</div>;
-    }
+	if (error) {
+		return <div>error:{error}</div>
+	}
 
-    return (
-        <div>
-            {discipline}
-            <div>
-                <RoadMap />
-            </div>
-        </div>
-    );
-};
+	return (
+		<div>
+			{discipline}
+			<div className={styles.container}>
+				<RoadMap />
+			</div>
+		</div>
+	)
+}
 
-export default Roadmap;
+export default Roadmap
--- src/shared/CustomNode/CustomNode.tsx
+++ src/shared/CustomNode/CustomNode.tsx
@@ -1,44 +1,48 @@
-import React from "react";
-import styles from "./CustomNode.module.scss";
-import { Handle, Position } from "reactflow";
+import React from 'react'
+import styles from './CustomNode.module.scss'
+import { Handle, Position } from 'reactflow'
 
 interface CustomNodeProps {
-    data: {
-        label: string;
-        isFirst: boolean;
-        isLast: boolean;
-        sources: boolean;
-    };
+	data: {
+		label: string
+		isFirst: boolean
+		isLast: boolean
+		topics: number
+	}
 }
 
 const CustomNode: React.FC<CustomNodeProps> = ({
-    data: { label, isFirst, isLast, sources },
+	data: { label, isFirst, isLast, topics },
 }) => {
-    return (
-        <>
-            {!isFirst && (
-                <Handle type="target" position={Position.Top} id="right" />
-            )}
-            {sources ? (
-                <>
-                    <Handle type="source" position={Position.Left} id="left" />
-                    <Handle
-                        type="source"
-                        position={Position.Right}
-                        id="right"
-                    />
-                </>
-            ) : (
-                <Handle type="source" position={Position.Left} id="left" />
-            )}
-            <div className={styles.main}>
-                <div> {label}</div>
-            </div>
-            {!isLast && (
-                <Handle type="source" position={Position.Bottom} id="bottom" />
-            )}
-        </>
-    );
-};
+	return (
+		<>
+			{!isFirst && (
+				<Handle type='target' position={Position.Top} id='top' />
+			)}
+			{topics === 0 ? (
+				<></>
+			) : topics > 1 ? (
+				<>
+					<Handle type='source' position={Position.Left} id='left' />
+					<Handle
+						type='source'
+						position={Position.Right}
+						id='right'
+					/>
+				</>
+			) : (
+				<>
+					<Handle type='source' position={Position.Left} id='left' />
+				</>
+			)}
+			<div className={styles.main}>
+				<div> {label}</div>
+			</div>
+			{!isLast && (
+				<Handle type='source' position={Position.Bottom} id='bottom' />
+			)}
+		</>
+	)
+}
 
-export default CustomNode;
+export default CustomNode
